bde60f5c7254a0a8787e2f69e6d720643846149a
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-08-31 07:03:50 -0700

    Update ExampleUnitTest.java

--- app/src/test/java/com/laioffer/tinnews/ExampleUnitTest.java
+++ app/src/test/java/com/laioffer/tinnews/ExampleUnitTest.java
@@ -3,7 +3,6 @@ package com.laioffer.tinnews;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
